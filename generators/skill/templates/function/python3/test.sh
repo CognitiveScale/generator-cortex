f06125991ac089055b805b36470395821dbe2808
Johan Gielstra <jgielstra@cognitivescale.com>
2018-02-13 15:40:37 -0600

    Use default profile in cortex cli, bump to 0.2.2

--- generators/skill/templates/function/python3/test.sh
+++ generators/skill/templates/function/python3/test.sh
@@ -2,4 +2,4 @@
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cortex functions invoke <%= projectPrefix %>_<%= functionName %> --params-file "${SCRIPT_DIR}/test/test_req.json" --profile dev
+cortex functions invoke <%= projectPrefix %>_<%= functionName %> --params-file "${SCRIPT_DIR}/test/test_req.json"
