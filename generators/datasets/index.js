d3031fc89362699d4c71f44b0fb4f9caa72fa46b
Dave Wisecup <dwisecup@cognitivescale.com>
2018-03-05 16:43:53 -0600

    working dynamic list of existing connections and types.

--- generators/datasets/index.js
+++ generators/datasets/index.js
@@ -15,71 +15,102 @@
  */
 
 
+const Config = require('../client/config');
+
+const debug = require('debug')('cortex:cli');
 const Connections = require("../client/connections");
+const Catalog = require("../client/catalog");
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
 
-    prompting() {
-      const options = this.options.cortexOptions;
-      console.log(`options: ${options}`);
-      const profileName = options.profile || 'default';
-      const conn = new Connections(options.url);
-      const listOfConns = conn.listConnections(options.token);
-
-      console.log(`conns: ${JSON.stringify(listOfConns)}`);
-
-      return this.prompt([
-      {
-        type    : 'input',
-        name    : 'datasetName',
-        message : 'Dataset Name'
-      },
-      {
-        type    : 'input',
-        name    : 'title',
-        message : 'Title'
-      },
-      {
-        type    : 'input',
-        name    : 'description',
-        message : 'Description'
-      },
-      {
-        type    : 'input',
-        name    : 'connectionName',
-        message : 'Connetion Name'      // TODO: grab dynamic list from list connections
-      },
-      {
-        type    : 'input',
-        name    : 'typeName',
-        message : 'Type Name'      // TODO: grab dynamic list from list types
-      }]).then((answers) => {
-        this.options.projectName    = this.config.get('projectName');
-        this.options.projectPrefix  = this.config.get('projectPrefix');
-        this.options.datasetName    = answers.datasetName;
-        this.options.title          = answers.title.trim();
-        this.options.description    = answers.description.trim();
-        this.options.connectionName = answers.connectionName;
-        this.options.typeName       = answers.typeName;
-      });
+    constructor(args, opts) {
+        super(args, opts);
+        this.connNameTypePair;
+        this.catalogTypes;
     }
 
-    writing() {
-        // TODO before we create yml check dataset endpoint for name uniqueness.
+    displayNames() {
+       return  this.connNameTypePair.map(i => i.name);
+    }
 
-        const connDetails = 'connectionQuery:\n  - name: query\n    value: --Insert SQL query--\n';
-        this.options.connDetails = connDetails;
+    prompting() {
+        const profileName = this.options.cortexProfile;
+        debug(`profileName: ${profileName}`);
+        const config = Config.readConfig();
+        debug('configuring profile: %s', profileName);
+        const profile = (config && config.getProfile(profileName)) || {};
 
-        const regex = /mongo/i;
-        const match = regex.exec(this.options.connectionName);
-        if (match) {
-            const connDetails = 'connectionQuery:\n  - name: collection\n    value: --Insert Collection name--\n';
-            const connDetailsMongo = '  - name: filter\n    value: --Insert Collection filter {}--\n';
-            this.options.connDetails = connDetails + connDetailsMongo;
-        }
+        const conn = new Connections(profile.url);
+        const getConns = conn.listConnections(profile.token).then((response) => {
+            this.connNameTypePair = response.result.connections.map(item => {
+                    return {'name': item.name, 'connType': item.connectionType};
+                }
+            );
+        });
 
-        const commonPath = 'common/**/*';
+        const types = new Catalog(profile.url);
+        const getTypes = types.listTypes(profile.token).then((response) => {
+            this.catalogTypes = response.types.map(item => item.name);
+        });
+        const ops = [getConns, getTypes];
+
+        return Promise.all(ops).then((response) => {
+            return this.prompt([
+                {
+                  type    : 'input',
+                  name    : 'datasetName',
+                  message : 'Dataset Name'
+                },
+                {
+                  type    : 'input',
+                  name    : 'title',
+                  message : 'Title'
+                },
+                {
+                  type    : 'input',
+                  name    : 'description',
+                  message : 'Description'
+                },
+                {
+                  type    : 'list',
+                  name    : 'connectionName',
+                  message : 'Connetion Name',
+                  choices : this.displayNames()
+                },
+                {
+                  type    : 'list',
+                  name    : 'typeName',
+                  message : 'Type Name',
+                  choices : this.catalogTypes
+                }]).then((answers) => {
+                  this.options.projectName    = this.config.get('projectName');
+                  this.options.projectPrefix  = this.config.get('projectPrefix');
+                  this.options.datasetName    = answers.datasetName;
+                  this.options.title          = answers.title.trim();
+                  this.options.description    = answers.description.trim();
+                  this.options.connectionName = answers.connectionName;
+                  this.options.typeName       = answers.typeName;
+                });
+
+        });
+    }
+
+      writing() {
+          // TODO before we create yml check dataset endpoint for name uniqueness.
+
+          const connDetails = 'connectionQuery:\n  - name: query\n    value: --Insert SQL query--\n';
+          this.options.connDetails = connDetails;
+
+          const regex = /mongo/i;
+          const match = regex.exec(this.options.connectionName);
+          if (match) {
+              const connDetails = 'connectionQuery:\n  - name: collection\n    value: --Insert Collection name--\n';
+              const connDetailsMongo = '  - name: filter\n    value: --Insert Collection filter {}--\n';
+              this.options.connDetails = connDetails + connDetailsMongo;
+          }
+
+          const commonPath = 'common/**/*';
         const connDir = this.destinationPath('dataset/' + this.options.datasetName);
         this.log('Creating connection', this.options.datasetName, 'in', connDir);
         this.fs.copyTpl( this.templatePath(commonPath), connDir, this.options);
