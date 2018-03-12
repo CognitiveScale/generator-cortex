9ec4972b46cbafc2e3e69ccac74d8974a0d57da7
Dave Wisecup <dwisecup@cognitivescale.com>
2018-03-12 16:14:52 -0500

    fix for deploy script

--- generators/connections/templates/common/deploy-connection.sh
+++ generators/connections/templates/common/deploy-connection.sh
@@ -3,4 +3,4 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Deploy our skill to Cortex
-cortex connections save --yaml connection.yaml
+cortex connections save --yaml $SCRIPT_DIR/connection.yaml
--- generators/datasets/templates/common/deploy-dataset.sh
+++ generators/datasets/templates/common/deploy-dataset.sh
@@ -3,4 +3,4 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Deploy our skill to Cortex
-cortex datasets save --yaml dataset.yaml
+cortex datasets save --yaml $SCRIPT_DIR/dataset.yaml
