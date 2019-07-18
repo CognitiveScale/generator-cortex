e727d0d057e732f91ece64110dc69dd6c176060c
clee <clee@cognitivescale.com>
2019-07-18 12:40:09 -0500

    Revert "Bring in generator 0.3.20 (#45)"
    This reverts commit d2c5c3ebaeb9f47a1e7f578fde264e916be0a67d.
    
    # Conflicts:
    #	package.json

--- generators/skill/templates/daemon/scripts/common/win32/test-daemon.bat
+++
@@ -1,7 +0,0 @@
-echo off
-SET SCRIPT_DIR=%~dp0
-SET SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
-SET TEST_FILE=%SCRIPT_DIR%\test\test_req.json
-
-echo Invoking <%= projectPrefix %><%= functionName %> in Cortex...
-cortex actions invoke <%= projectPrefix %><%= functionName %> --params-file "%TEST_FILE%"
