e9f065c24372f4f0dbf1ef2d4e022fa29a6c2867
Mayank <mchutani@cognitivescale.com>
2019-07-24 20:37:28 +0530

    added ps1 file

--- generators/datasets/templates/scripts/win32/buildzip.ps1
+++ generators/datasets/templates/scripts/win32/buildzip.ps1
@@ -28,4 +28,3 @@ class FixedEncoder : System.Text.UTF8Encoding {
 echo "Building zip $dstFile"
 
 [System.IO.Compression.ZipFile]::CreateFromDirectory($srcFolder, $dstFile, [System.IO.Compression.CompressionLevel]::Optimal, $false, [FixedEncoder]::new())
-
